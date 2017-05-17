057158230111aa3c4072a146bcc467bb585e5fc3
Macarena Baltra <macarena.bc2011@gmail.com>
2017-05-17 14:04:19 -0400

    Subiendo lo que se alcanso parte 0

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Boton 1 para guardar nueva tarea.
+function mostrarYNo(){
+  var mostrar = document.getElementById('doton');
+  if(mostrar.style.display === 'none')
+  mostrar.style.display = 'block';
+}
+
+function fueraDeMi(){
+  var ocultar = document.getElementById('doton');
+  if(mostrar.style.display = 'block')
+  mostrar.style.display = 'none';
+}
