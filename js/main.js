3270c096484a80e37778dc5a50bf2e9bfca4da22
Macarena Baltra <macarena.bc2011@gmail.com>
2017-05-17 22:20:44 -0400

    Subiendo Ejercicio u.u - Atroz!

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
 // Boton 1 para guardar nueva tarea.
 function mostrarYNo(){
   var mostrar = document.getElementById('doton');
-  if(mostrar.style.display === 'none')
-  mostrar.style.display = 'block';
+    if(mostrar.style.display === 'none')
+    mostrar.style.display = 'block';
+
 }
 
+/* No Funciona T.T
 function fueraDeMi(){
   var ocultar = document.getElementById('doton');
   if(mostrar.style.display = 'block')
   mostrar.style.display = 'none';
 }
+*/
+
+/*
+  var ingreso = document.getElementById("ingresoDato");
+  var datoInCaja = document.getElementById("cajaDatos");
+
+      mostrar.addEventListener("click", function(){
+      var cajaList = document.createElement("div");
+      cajaList.classList.add('datoCaja');
+*/
+  
+/*
+function agregar(){
+  var cajaDatos = document.getElementById('');
+}
+*/
